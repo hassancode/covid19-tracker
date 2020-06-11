156cdd51ddc27b0934f5c9a35cdbc5945229df4a
hassancode <syedhassanali85@gmail.com>
2020-06-11 19:23:30 -0400

    removed unused imports

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,9 +3,7 @@ import React, { useState, useEffect } from 'react';
 const Chart = () => {
     const [dailyData, setDailyData] = useState({});
 
-    // useEffect(async () => {
-    //     setDailyData(api.fetchDailyData());
-    // });
+   
     return (
         <h1>Chart</h1>
     )
