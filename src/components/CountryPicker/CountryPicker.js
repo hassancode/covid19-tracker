055dc33ded9f6709b305f3fba4fb511d8cea5c31
hassancode <syedhassanali85@gmail.com>
2020-06-12 21:44:50 -0400

    removed unused code

--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -3,7 +3,7 @@ import { fetchCountries } from '../../api';
 import styles from './CountryPicker.module.css'
 import { NativeSelect, FormControl } from '@material-ui/core';
 
-const CountryPicker = ({handleCountryChange}) => {
+const CountryPicker = ({ handleCountryChange }) => {
     const [countries, setCountries] = useState([]);
 
     useEffect(() => {
@@ -11,38 +11,17 @@ const CountryPicker = ({handleCountryChange}) => {
             const data = await fetchCountries();
             setCountries(data);
             console.log(countries);
-        }; 
+        };
         fetchApi();
     }, []);
 
-    const onChange = (event) => {
-        alert(event.target.value)
-    }
-
     return (
-        // countries.length ?
-        //     (
-        //         <select onChange={onChange}>
-        //             {countries.map(country => (
-        //                 <option key={country.name}>{country.name}</option>
-        //             ))}
-        //         </select>
-        //     )
-        //     : null
-        // <FormControl className={styles.formcontrol}>
-        //     <NativeSelect>
-        //         <option value="global">global</option> 
-
-        //         {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
-        //     </NativeSelect>
-        // </FormControl>
-
         <FormControl className={styles.formControl}>
-      <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
-        <option value="">Global</option>
-        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
-      </NativeSelect>
-    </FormControl>
+            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
+                {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
